884a546090b86153b1b619adbffa967fef3129b5
Mark May <mamark90@gmail.com>
2019-06-07 14:24:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,13 +9,11 @@ class Dog
   def initialize(name)
     @name = name
     @@all << self
-    binding.pry
   end
 
 def self.all
   @@all.select{|n| n=~/[name=]\w/}
   binding.pry
-  end
 end
 
   def self.clear_all
